cac2abd23cd929078d0da4265bc4e98575ed215e
WangYonghao <116246102+yohoo11@users.noreply.github.com>
2023-04-09 09:51:50 +0800

    Add files via upload

---
+++ c++基本框架.cpp
@@ -0,0 +1,20 @@
+//#include<iostream> //i-input o-output stream-流
+////iostream  istream(cin)/ostream输出类(cout对象)
+//using namespace std; //使用标准命名空间std  
+////限制cin/cout的使用范围在std下
+//namespace jj {
+//	int a = 1;
+//}
+//namespace gg {
+//	int a = 2;
+//}
+////自定义命名空间，防止同名变量或者同名函数的重定义
+//using namespace jj;
+//using namespace gg;
+//
+//int main() {
+//	cout << jj::a;      //::作用域符号
+//	cout << "hello world";
+//	//cout输出    <<重载操作符
+//	return 0;
+//}
---
+++ c++新增类型.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	//布尔类型 两个值1/0    true（非0）/false（0）
+	bool b = 0;
+	cout << b << endl << sizeof(b) << "字节" << endl;
+	return 0;
+}
